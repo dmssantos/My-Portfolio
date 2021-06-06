a9302ce221dd013029ee069683bc1c12a161e8c0
dmssantos <dms.santos_@hotmail.com>
2021-06-06 13:25:31 -0300

    Final Commit

--- script.js
+++ script.js
@@ -44,5 +44,9 @@ function reveal(){
     var windowHeight = window.innerHeight
     var revealTop = reveals[i].getBoundingClientRect().top
     var revealPoint = 50
+
+    if(revealTop < windowHeight - revealPoint){
+      reveals[i].classList.add("active")
+    }
   }
 }
